f02b820f86c3484dfe482b6c67a7df91bb6ff874
Skaidrius <skaidrius@gmail.com>
2017-01-04 08:14:33 +0000

    styling recommendations

--- vadis/src/components/recs/recommendations.vue
+++ vadis/src/components/recs/recommendations.vue
@@ -5,17 +5,17 @@
     <div class="jumbotron">
       <div class="row">
         <div class='col-xs-10'>
-          <h2>{{ pageTitle.jumboHead[i18n] }}<br><small>{{ pageTitle.jumboSmall[i18n] }}</small></h2>
+          <h2>{{ pageTitle.jumboHead[ i18n ] }}<br><small>{{ pageTitle.jumboSmall[ i18n ] }}</small></h2>
     
-          <h4>{{pageTitle.todayIs[i18n]}} {{today}} </h4>
+          <h4>{{pageTitle.todayIs[ i18n ]}} {{ today }} </h4>
         </div>
         
         <div class='col-xs-2'>
           <br/><br/>
           <div class='form-inline text-right'>
             <small>
-              <span v-if='editMode' class='btn-danger '>{{ header.mode.edit[i18n] }}</span>
-              <span v-else class='btn-success '>{{ header.mode.regular[i18n] }}</span>
+              <span v-if='editMode' class='btn-danger '>{{ header.mode.edit[ i18n ] }}</span>
+              <span v-else class='btn-success '>{{ header.mode.regular[ i18n ] }}</span>
             </small>
           </div>
 
@@ -31,11 +31,11 @@
     
     <nav class='container-fluid'>
       <ul class="nav nav-pills text-center my-tabs row">
-        <li class='col-xs-2 active' data-toggle="tab"><router-link to="/recommendations/actual">{{ tables.activeRec[i18n] }}</router-link></li>
-        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/late">{{ tables.lateRec[i18n] }}</router-link></li>
-        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/implemented">{{ tables.impleRec[i18n] }}</router-link></li>
-        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/add">{{ functions.titles.addNew[i18n] }}</router-link></li>
-        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/search">{{ functions.search[i18n] }}</router-link></li>
+        <li class='col-xs-2 active' data-toggle="tab"><router-link to="/recommendations/actual">{{ tables.activeRec[ i18n ] }}</router-link></li>
+        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/late">{{ tables.lateRec[ i18n ] }}</router-link></li>
+        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/implemented">{{ tables.impleRec[ i18n ] }}</router-link></li>
+        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/add">{{ functions.titles.addNew[ i18n ] }}</router-link></li>
+        <li class='col-xs-2' data-toggle="tab"><router-link to="/recommendations/search">{{ functions.search[ i18n ] }}</router-link></li>
       </ul>
       </nav>
 
@@ -89,10 +89,10 @@ module.exports = {
         header: apiData.table.header,
       };
     },
-    props: ['i18n', 'editMode'],
+    props: [ 'i18n', 'editMode' ],
     computed: {
-      rated: function (idx){
-        return this.functions.recRates[idx][this.i18n];
+      rated: function ( idx ){
+        return this.functions.recRates[ idx ][ this.i18n ];
       },
 
       today: function() {
@@ -105,42 +105,42 @@ module.exports = {
         let thisDay = td.getDate();
         let tempDay;
         
-        const monthsLt = ["sausio", "vasario", "kovo", "balandžio", "gegužės", "birželio", "liepos", "rugpjūčio", "rugsėjo", "spalio", "lapkričio", "gruodžio"];
-        const monthsEn = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const monthsLt = [ "sausio", "vasario", "kovo", "balandžio", "gegužės", "birželio", "liepos", "rugpjūčio", "rugsėjo", "spalio", "lapkričio", "gruodžio" ];
+        const monthsEn = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
         
         //if (thisMonth < 10) thisMonth = "0" + thisMonth;
         //if (thisDay < 10) thisDay = "0" + thisDay;
         
-        if (i18n == 'lt'){
+        if ( i18n == 'lt' ){
           // lithuanian date format
-          str += thisYear + " m. " + monthsLt[thisMonth] + " " + thisDay + " d.";
+          str += thisYear + " m. " + monthsLt[ thisMonth ] + " " + thisDay + " d.";
         } else {
           // us date format 
           if (thisDay > 10 && thisDay < 14 ) {
             tempDay = thisDay + 'th';
-          } else if (thisDay % 10 == 1) {
+          } else if ( thisDay % 10 == 1 ) {
             tempDay = thisDay + 'st';
-          } else if (thisDay % 10 == 2) {
+          } else if ( thisDay % 10 == 2 ) {
             tempDay = thisDay + 'nd';
-          } else if (thisDay % 10 == 3) {
+          } else if ( thisDay % 10 == 3 ) {
             tempDay = thisDay + 'rd';
           } else {
             tempDay = thisDay + 'th';
           }
-          str += tempDay + ' of ' + monthsEn[thisMonth] + ', ' + thisYear; 
+          str += tempDay + ' of ' + monthsEn[ thisMonth ] + ', ' + thisYear; 
         }
         return str;
       }
 
     },
     methods: {
-      sortByRate: function(el){ //currently sorts middle/high by letter h l m, not by value l=1, m = 2, h = 3
+      sortByRate: function( el ){ //currently sorts middle/high by letter h l m, not by value l=1, m = 2, h = 3
         this.sorted *=-1;
-        return this.recommendationsArray.sort( (a, b) => a.recommendations.recRate > b.recommendations.recRate ? this.sorted : this.sorted*-1 );
+        return this.recommendationsArray.sort( ( a, b ) => a.recommendations.recRate > b.recommendations.recRate ? this.sorted : this.sorted*-1 );
       },
-      sortByDate: function(el){
+      sortByDate: function( el ){
         this.sorted *=-1;
-        return this.recommendationsArray.sort( (a, b) => a.recommendations.period > b.recommendations.period ? this.sorted : this.sorted*-1 );
+        return this.recommendationsArray.sort( ( a, b ) => a.recommendations.period > b.recommendations.period ? this.sorted : this.sorted*-1 );
       }
     }
 };
@@ -167,7 +167,7 @@ table {
     left: -2050px;
   }
   
-  .expand input[type=checkbox]:before { 
+  .expand input[ type=checkbox ]:before { 
     position: relative;
     left: 2050px;
     content: "\003E";
@@ -176,7 +176,7 @@ table {
     color: OliveDrab ;
   }
   
-  .expand input[type=checkbox]:checked:before {
+  .expand input[ type=checkbox ]:checked:before {
     content: "\2227";
     font-weight: bold;
     color: DarkCyan;
