a9ea78ce8f1e1f75c996c9adc41cd5ffd70d0a6e
Skaidrius <skaidrius@gmail.com>
2017-01-04 10:59:39 +0000

    search rers done

--- vadis/src/components/recs/search-recs.vue
+++ vadis/src/components/recs/search-recs.vue
@@ -7,8 +7,9 @@
       <div class="panel-body">
         <h2> {{ functions.search[i18n] }} </h2>
         <p>
-          <input v-model="userInput" class="form-control" :placeholder="functions.search[i18n]"/>
+            <input v-model="userInput" class="form-control" :placeholder="functions.search[i18n]"/>
         </p>
+
       </div>
 
       <div class='panel-heading'>
@@ -30,27 +31,72 @@
             <th @click='sortByStatus()'><a>{{ functions.status.title[i18n] }}</a></th>
           </tr>
         </thead>
-        <tbody>
-          <tr v-for='(data, index) in filteredElements'>
-            <td>{{ index+1 }}.</td>
-            <td v-for='(el, key) in data.recommendations' v-show='key!=="actual"'>
-              <span v-if='key=="recRate"'>{{ functions.recRates[el-1][i18n] }}</span> <!-- to show low/med/high instead of 1,2,3 -->
-              <span v-else>{{ el }}</span>
-            </td>
-            <td>{{ data.recommendations.actual ? functions.status.actual[i18n] : functions.status.implemented[i18n] }}</td>
-          </tr>
-        </tbody>
+
+        <tbody v-for='( data, index ) in filteredElements'>
+            <tr>
+              <td>{{ index+1 }}.</td>
+              <td v-for='( el, key ) in data.recommendations' v-show='key!=="actual"' class='with-innertable'>
+                <!--SPLIT CELL TO TEXT AND EXPAND symbol -->
+                <table class='table innertable'>
+                  <tr class='row'>
+                    <td class='col-xs-11'>
+                      <span v-if='key=="recRate"'>{{ functions.recRates[el-1][i18n] }}</span> <!-- to show low/med/high instead of 1,2,3 -->
+                      <span v-else-if='editMode && key=="implementInfo"'>
+                        <textarea v-model='data.recommendations[ key ]' :placeholder='data.recommendations[ key ]' class='form-control'></textarea>
+                      </span>
+                      <span v-else>{{ el }}</span>
+                    </td>
+                    <!--input checkbox = arrow-->
+                    <td class='col-xs-1 expand expand-symbol'>
+                      <input type='checkbox' class='check_box' v-on:click='expandRec( data )' v-model='data.checked'/>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+              <!-- RECS mark as selected - -->
+              <td>{{ data.recommendations.actual ? functions.status.actual[i18n] : functions.status.implemented[i18n] }}</td>
+            </tr>
+            
+            <!--EXPAND INFO ABOUT IMPLEMENTATION ON MARK-->
+            <tr v-if='data.marked'>
+              <td></td>
+              <td colspan='8'>
+                <table class='table innertable'>
+                  <tr>
+                    <th class='text-center'>{{ functions.describeImplementation[ i18n ]}}</th>
+                  </tr>
+                  <tr>
+                    <td><textarea class='form-control' v-model='data.recommendations.implementInfo'></textarea></td>
+                  </tr>
+                </table>
+              </td>
+              
+            </tr>
+
+            <!--EXPAND RECOMMENDATIONS MODULE-->
+            <tr v-if='data.checked'>
+              <td colspan = '9'>
+                <expand-recs 
+                        :data='data' 
+                        :i18n='i18n'
+                        :editMode='editMode'
+                >
+                </expand-recs>
+              </td>
+            </tr>
+          </tbody>
+        
       </table>
 
       <div v-if='userInput!== "" && filteredElements.length === 0' class='panel-footer'>{{ functions.noRes[i18n] }}</div>
     </div>
-
   </div>
 </template>
 
 <script>
   
 const apiData = require('../../assets/recommend-table-data.json');
+import ExpandRecsModule from './modules/expand-recs-module.vue';
 
 module.exports =  {
   data(){
@@ -65,6 +111,9 @@ module.exports =  {
       sorted: true,
     };
   },
+  components: {
+    expandRecs: ExpandRecsModule
+  },
   computed:{
     filteredElements: function () {
       return this.recommendationsArray
@@ -73,10 +122,27 @@ module.exports =  {
     },
   },
   methods: {
+      getMarked: function () {
+        let temp = 0;
+        this.filteredElements.map( function ( e ) {
+          if ( e.marked ) {
+            temp++;
+          }
+        });
+        this.marked = temp; 
+      },
       sortByStatus: function(){
         this.sorted *=-1;
         return this.recommendationsArray.sort((a, b) => a.recommendations.actual > b.recommendations.actual ? this.sorted : this.sorted*-1 );
-      }
+      },
+      // checks whether any rec is checked 
+      expandRec: function ( data ) {
+        this.filteredElements.map( function ( e ) {
+          if ( e == data ) {
+            e.checked = e.checked ? false : true;
+          } else { e.checked = false; }
+        });
+    }
   },
   props: ['i18n', 'editMode', 'functions', 'tables', 'sortByDate', 'sortByRate','recommendationsArray', 'tableElements']
 };
